7edcfba417e329387ef5e20ca5336f66667e82b5
aso4 <albertyso@gmail.com>
2017-02-17 13:52:41 -0500

    clean up commented code and fix link

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,13 +1,8 @@
  (function() {
      function CollectionCtrl(Fixtures) {
          this.albums = Fixtures.getCollection(12);
-         /*
-         this.albums = [];
-         for (var i=0; i < 12; i++) {
-            this.albums.push(angular.copy(albumPicasso));
-         } */
      }
- 
+
      angular
          .module('blocJams')
          .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
