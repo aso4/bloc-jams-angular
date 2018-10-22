20919c643776745539eabf51f0556af1d096cd2a
aso4 <albertyso@gmail.com>
2018-10-22 08:11:37 -0400

    server.js rewrite for hapi 11.1.4

--- server.js
+++ server.js
@@ -1,43 +1,53 @@
-var Hapi = require('hapi'),
-    path = require('path'),
-    port = process.env.PORT || 5000,
-    server = new Hapi.Server(port),
-    routes = {
-        css: {
-            method: 'GET',
-            path: '/styles/{path*}',
-            handler: createDirectoryRoute('styles')
-        },
-        js: {
-            method: 'GET',
-            path: '/scripts/{path*}',
-            handler: createDirectoryRoute('scripts')
-        },
-        assets: {
-            method: 'GET',
-            path: '/assets/{path*}',
-            handler: createDirectoryRoute('assets')
-        },
-        templates: {
-            method: 'GET',
-            path: '/templates/{path*}',
-            handler: createDirectoryRoute('templates')
-        },
-        spa: {
-            method: 'GET',
-            path: '/{path*}',
-            handler: {
-                file: path.join(__dirname, '/dist/index.html')
-            }
-        }
-    };
-
-server.route([ routes.css, routes.js, routes.assets, routes.templates, routes.spa ]);
-server.start( onServerStarted );
-
-function onServerStarted() {
-    console.log( 'Server running on port ', port );
-}
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({ port: process.env.PORT || 3000, host: 'localhost'});
+const path = require('path');
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+//
+//  = {
+//         css: {
+//             method: 'GET',
+//             path: '/styles/{path*}',
+//             handler: createDirectoryRoute('styles')
+//         },
+//         js: {
+//             method: 'GET',
+//             path: '/scripts/{path*}',
+//             handler: createDirectoryRoute('scripts')
+//         },
+//         assets: {
+//             method: 'GET',
+//             path: '/assets/{path*}',
+//             handler: createDirectoryRoute('assets')
+//         },
+//         templates: {
+//             method: 'GET',
+//             path: '/templates/{path*}',
+//             handler: createDirectoryRoute('templates')
+//         },
+//         spa: {
+//             method: 'GET',
+//             path: '/{path*}',
+//             handler: {
+//                 file: path.join(__dirname, '/dist/index.html')
+//             }
+//         }
+//     };
+//
+// server.route([ routes.css, routes.js, routes.assets, routes.templates, routes.spa ]);
+// server.start( onServerStarted );
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log( 'Server running on port ' );
+})
 
 function createDirectoryRoute( directory ) {
     return {
