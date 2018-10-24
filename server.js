0970b137af06c9d2f19a514d76efc060fe30fbc8
aso4 <albertyso@gmail.com>
2018-10-23 23:41:11 -0400

    server.js with routes array, index.html still non-working

--- server.js
+++ server.js
@@ -3,52 +3,56 @@ const server = new Hapi.Server();
 const port = process.env.PORT || 3000;
 server.connection({ port: port, host: 'localhost'});
 const path = require('path');
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply(path.join(__dirname, '/dist/index.html'));
-  }
-});
+// server.route([
+//   {
+//   method: 'GET',
+//   path: '/',
+//   handler: function (request, reply) {
+//     reply(path.join(__dirname, '/dist/index.html'));
+//   }},
+//   {
+//     method: 'GET',
+//     path: '/styles/{path}',
+//     handler: function (request, reply) {
+//       reply(createDirectoryRoute('styles'));
+//   }}
+// ]);
 
-server.route({
-  method: 'GET',
-  path: '/styles/{path}',
-  handler: function (request, reply) {
-    reply(createDirectoryRoute('styles'));
-  }
-});
+const handler = function (request, reply) {
+    return reply({ status: 'ok' });
+};
 
-//         css: {
-//             method: 'GET',
-//             path: '/styles/{path*}',
-//             handler: createDirectoryRoute('styles')
-//         },
-//         js: {
-//             method: 'GET',
-//             path: '/scripts/{path*}',
-//             handler: createDirectoryRoute('scripts')
-//         },
-//         assets: {
-//             method: 'GET',
-//             path: '/assets/{path*}',
-//             handler: createDirectoryRoute('assets')
-//         },
-//         templates: {
-//             method: 'GET',
-//             path: '/templates/{path*}',
-//             handler: createDirectoryRoute('templates')
-//         },
-//         spa: {
-//             method: 'GET',
-//             path: '/{path*}',
-//             handler: {
-//                 file: path.join(__dirname, '/dist/index.html')
-//             }
-//         }
-//     };
-//
-// server.route([ routes.css, routes.js, routes.assets, routes.templates, routes.spa ]);
+const routes = {
+        // css: {
+        //     method: 'GET',
+        //     path: '/styles/{path*}',
+        //     handler: createDirectoryRoute('styles')
+        // },
+        // js: {
+        //     method: 'GET',
+        //     path: '/scripts/{path*}',
+        //     handler: createDirectoryRoute('scripts')
+        // },
+        // assets: {
+        //     method: 'GET',
+        //     path: '/assets/{path*}',
+        //     handler: createDirectoryRoute('assets')
+        // },
+        // templates: {
+        //     method: 'GET',
+        //     path: '/templates/{path*}',
+        //     handler: createDirectoryRoute('templates')
+        // },
+        spa: {
+            method: 'GET',
+            path: '/{path*}',
+            handler: function (request, reply) {
+                 reply(path.join(__dirname, '/dist/index.html'));
+            }
+        }
+    };
+
+server.route([ routes.spa ]);
 
 server.start((err) => {
   if (err) {
